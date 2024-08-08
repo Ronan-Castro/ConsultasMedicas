1b99ba3991c6817bf719d1f1df5b8754b563647e
Ronan De Souza Castro <netherrock75@gmail.com>
2024-08-08 10:38:34 -0300

    Commit Inicial - Modelos, DbSet e Seeds

---
+++ ConsultaMedica/ConsultaMedica/ConsultaMedica/Data/Mapping/PacienteMap.cs
@@ -0,0 +1,40 @@
+﻿using ConsultaMedica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ConsultaMedica.Data.Mapping
+{
+    public class PacienteMap : IEntityTypeConfiguration<Paciente>
+    {
+        public void Configure(EntityTypeBuilder<Paciente> builder)
+        {
+            builder.ToTable("Pacientes");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR(50)");
+
+            builder.Property(x => x.Documento)
+                .IsRequired(true)
+                .HasColumnType("NVARCHAR(11)");
+
+            builder.Property(x => x.Email)
+                .IsRequired(true)
+                .HasColumnType("VARCHAR(50)");
+
+            builder.Property(x => x.Celular)
+                .IsRequired(true)
+                .HasColumnType("NVARCHAR(11)");
+
+            builder.HasIndex(x => x.Documento)
+                .IsUnique();
+
+            builder.HasMany(a=>a.Agendamentos)
+                .WithOne(p=>p.Paciente)
+                .HasForeignKey(a=>a.PacienteId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
